ebf02c0165f7067cd2b01a46c67dca96a8fcecbe
Iftakhar Husan <iftakharn19@gmail.com>
2024-01-29 13:44:45 +0200

    Add naive builder

---
+++ Builder/NaiveBuilder.py
@@ -0,0 +1,17 @@
+"""
+Here we have a naive builder that builds HTML markup.
+This is error prone and not very easy.
+"""
+
+text = 'hello'
+parts = ['<p>', text, '</p>']
+print(''.join(parts))
+
+
+# now, imagine you want to build a list with 50 items
+words = ['hello', 'world'] * 25
+parts = ['<ul>']
+for w in words:
+	parts.append(f'  <li>{w}</li>')
+parts.append('</ul>')
+print('\n'.join(parts))
